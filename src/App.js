4fefc7a0101905053dd10c2697c73c2fdc356a93
Jorge Gonzalez <jglez.cs@gmail.com>
2020-12-11 12:33:57 -0600

    Successfully retrieved API data, rendered the array, and created a styled component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get('https://rickandmortyapi.com/api/character')
+      .then(res => {
+        setCharacters(res.data.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  // const openDetails = id => {
+  //   setCurrentCharacter(id)
+  // }
+
+  // const closeDetails = () => {
+  //   setCurrentCharacter(null)
+  // }
+
+  const StyledCharacter = styled.p`
+    font-weight: ${props => props.theme.weight};
+    color: #fff;
+    text-shadow: 0 0 20px #ff005b;
+    font-size: ${props => props.theme.fontSize};
+
+    &:hover {
+      color: ${props => props.theme.octonaryColor};
+    }
+  `
+
+  const Character = props => (
+    <StyledCharacter>
+      {props.info.name}
+    </StyledCharacter>
+  )
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Rick and Morty Characters</h1>
+      {
+        characters.map(char => {
+          return <Character key={char.id} info={char}/>
+        })
+      }
     </div>
   );
 }
---
+++ src/theme/theme.js
@@ -0,0 +1,24 @@
+// Theme object
+export default {
+  primaryColor: 'crimson',
+  secondaryColor: 'dodgerblue',
+  tertiaryColor: 'seagreen',
+  quaternaryColor: 'red',
+  quinaryColor: 'blue',
+  senaryColor: 'green',
+  septenaryColor: 'darkred',
+  octonaryColor: 'blue',
+  black: 'black',
+  white: 'white',
+  weight: 'bold',
+  fontSize: '2em',
+  breakpoints: {
+    mobile: '(max-width: 550px)',
+    tablet: '(max-width: 800px)'
+  },
+  padding: {
+    small: '4px',
+    medium: '8px',
+    large: '12px'
+  }
+}
